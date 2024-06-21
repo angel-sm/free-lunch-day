1966c6f10aa884c278bb3a32692d348d7247708b
angel-sm <jose.angel.pacheco9729@gmail.com>
2024-06-21 08:08:33 -0500

    Fix

--- orders/src/server.ts
+++ orders/src/server.ts
@@ -2,17 +2,17 @@ import express, { type Request, type Response } from 'express'
 import type * as http from 'http'
 import httpStatus from 'http-status'
 import cors from 'cors'
-import { Controller } from './controllers'
 import { Repository } from './data-access/mongo.repository'
-import { PreprareRecipe } from './use-cases/prepare-recipe'
-import { RetriveRecipes } from './use-cases/retrieve-recipes'
+import { RegistryRecipeOrder } from './use-cases/registry-recipe-order.uc'
+import { FinishRecipeOrder } from './use-cases/finish-recipie-order.uc'
 import { SQS } from './utils/sqs.services'
+import { RetrieveOrders } from './use-cases/retrieve-orders.uc'
+import { Controller } from './controllers'
 
 export interface SQSQueues {
   recipesQueue: SQS
-  registryOrderQueue: SQS
+  registryResponseOrderQueue: SQS
   finishOrderQueue: SQS
-  ingredientsQueue: SQS
 }
 
 export class Server {
@@ -30,21 +30,19 @@ export class Server {
     )
 
     const repository = new Repository()
-    const preprareRecipe = new PreprareRecipe(repository)
-    const retrieveRecipes = new RetriveRecipes(repository)
+    new RegistryRecipeOrder(repository).run({
+      recipesQueue: sqs.recipesQueue,
+      registryResponseOrderQueue: sqs.registryResponseOrderQueue,
+    })
+    new FinishRecipeOrder(repository).run({
+      finishOrderQueue: sqs.finishOrderQueue,
+    })
 
-    const recipeController = new Controller(
-      preprareRecipe,
-      retrieveRecipes,
-      sqs,
-    )
+    const retrieveOrders = new RetrieveOrders(repository)
+    const controllers = new Controller(retrieveOrders)
 
-    this.express.get('/recipes', async (request, reply) =>
-      recipeController.retrieveRecipesHandler(request, reply),
-    )
-
-    this.express.post('/recipes', async (request, reply) =>
-      recipeController.prepareRecipeHandler(request, reply),
+    this.express.get('/orders', async (request, reply) =>
+      controllers.retrieveOrdersHandler(request, reply),
     )
 
     this.express.use(
@@ -52,31 +50,6 @@ export class Server {
         res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.message)
       },
     )
-  }
-
-  static async initSQS() {
-    const recipesQueue = new SQS('recipies_request', 'recipes_queue')
-    const registryOrderQueue = new SQS(
-      'orders_response',
-      'order_response_queue',
-    )
-    const finishOrderQueue = new SQS('orders_finish', 'order_finish_queue')
-    const ingredientsQueue = new SQS(
-      'ingredients_response',
-      'ingredients_response_queue',
-    )
-
-    await recipesQueue.connect()
-    await registryOrderQueue.connect()
-    await ingredientsQueue.connect()
-    await finishOrderQueue.connect()
-
-    return {
-      recipesQueue,
-      registryOrderQueue,
-      finishOrderQueue,
-      ingredientsQueue,
-    }
   }
 
   async listen(): Promise<void> {
@@ -89,6 +62,21 @@ export class Server {
         resolve()
       })
     })
+  }
+
+  static async initSQS() {
+    const recipesQueue = new SQS('recipies_request', 'recipes_queue')
+    const registryResponseOrderQueue = new SQS(
+      'orders_response',
+      'order_response_queue',
+    )
+    const finishOrderQueue = new SQS('orders_finish', 'order_finish_queue')
+
+    return {
+      recipesQueue,
+      registryResponseOrderQueue,
+      finishOrderQueue,
+    }
   }
 
   getHTTPServer() {
